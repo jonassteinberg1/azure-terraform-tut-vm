9aac805d776af60db61278e9dc70cec4146479bf
jonassteinberg1 <jsteinberg@breadfinance.com>
2021-06-12 16:16:27 -0500

    avail set init

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ resource "azurerm_resource_group" "rg" {
   name     = var.azurerm_rg_name
   location = var.azurerm_rg_location
 }
+
+resource "azurerm_availability_set" "as" {
+  name                = var.azurerm_as_name
+  location            = azure_resource_group.ag.location
+  resource_group_name = azure_resource_group.ag.name
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "azurerm_rg_location" {
   type    = string
   default = "eastus"
 }
+
+variable "azurerm_as_name" {
+  type    = string
+  default = "terraformTesting"
+}
