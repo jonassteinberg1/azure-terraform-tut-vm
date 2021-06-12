c62745a9772bc00f2557a6406454d6ac9df56701
jonassteinberg1 <jsteinberg@breadfinance.com>
2021-06-12 16:57:49 -0500

    rsa key

--- main.tf
+++ main.tf
@@ -7,37 +7,37 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_availability_set" "as" {
   name                = var.azurerm_as_name
-  location            = azurerm_resource_group.ag.location
-  resource_group_name = azurerm_resource_group.ag.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = join(" ", [azure_resource_group.rg.name, "-1"])
+  name                = join(" ", [azurerm_resource_group.rg.name, "-1"])
   address_space       = var.azurerm_vn_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "sn" {
-  name                 = join(" ", [azure_resource_group.rg.name, "-internal"])
+  name                 = join(" ", [azurerm_resource_group.rg.name, "-internal"])
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vent.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.azurerm_sn_address_prefixes
 }
 
 resource "azurerm_network_interface" "inet" {
-  name                = join(" ", [azure_resource_group.rg.name, "-internal-nic"])
+  name                = join(" ", [azurerm_resource_group.rg.name, "-internal-nic"])
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   ip_configuration {
-    name                          = join(" ", [azure_resource_group.rg.name, "-internal"])
-    subnet_id                     = azurerm_subnet.sn.subnet_id
+    name                          = join(" ", [azurerm_resource_group.rg.name, "-internal"])
+    subnet_id                     = azurerm_subnet.sn.id
     private_ip_address_allocation = var.azurerm_inet_address_allocation
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = join(" ", [azure_resource_group.rg.name, "-vm-1"])
+  name                = join(" ", [azurerm_resource_group.rg.name, "-vm-1"])
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
@@ -48,7 +48,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "root"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh/jonas.pem")
   }
 
   os_disk {
@@ -59,6 +59,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "credative"
     offer     = "Debian"
+    sku       = "10"
     version   = "latest"
   }
 }
