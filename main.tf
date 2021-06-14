8960b3fdb41ecbe7be7a86f3a068633cb283820e
jonassteinberg1 <jsteinberg@breadfinance.com>
2021-06-13 19:07:54 -0500

    v1.0

--- main.tf
+++ main.tf
@@ -12,43 +12,43 @@ resource "azurerm_availability_set" "as" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = join(" ", [azurerm_resource_group.rg.name, "-1"])
+  name                = join("", [azurerm_resource_group.rg.name, "-1"])
   address_space       = var.azurerm_vn_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "sn" {
-  name                 = join(" ", [azurerm_resource_group.rg.name, "-internal"])
+  name                 = join("", [azurerm_resource_group.rg.name, "-internal"])
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.azurerm_sn_address_prefixes
 }
 
 resource "azurerm_network_interface" "inet" {
-  name                = join(" ", [azurerm_resource_group.rg.name, "-internal-nic"])
+  name                = join("", [azurerm_resource_group.rg.name, "-internal-nic"])
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   ip_configuration {
-    name                          = join(" ", [azurerm_resource_group.rg.name, "-internal"])
+    name                          = join("", [azurerm_resource_group.rg.name, "-internal"])
     subnet_id                     = azurerm_subnet.sn.id
     private_ip_address_allocation = var.azurerm_inet_address_allocation
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = join(" ", [azurerm_resource_group.rg.name, "-vm-1"])
+  name                = join("", [azurerm_resource_group.rg.name, "-vm-1"])
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
-  admin_username      = "root"
+  admin_username      = "jonas"
   network_interface_ids = [
     azurerm_network_interface.inet.id,
   ]
 
   admin_ssh_key {
-    username   = "root"
-    public_key = file("~/.ssh/jonas.pem")
+    username   = "jonas"
+    public_key = file("~/.ssh/azure.pub")
   }
 
   os_disk {
@@ -57,8 +57,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   source_image_reference {
-    publisher = "credative"
-    offer     = "Debian"
+    publisher = "Debian"
+    offer     = "debian-10"
     sku       = "10"
     version   = "latest"
   }
